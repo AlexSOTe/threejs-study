1427bd716751e0479c11a74422cb6a77f0828636
Alex Liu <sotework@outlook.com>
2022-09-28 10:31:37 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="App">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .App {
+    width: 100vw;
+    height: 100vh;
+  }
+</style>
