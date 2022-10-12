7b9e861541da95348f9e6ded4360d177abf5ea34
Alex Liu <sotework@outlook.com>
2022-10-12 11:16:27 +0800

    修改项目配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,19 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+
+const basePath = '/sote-three/';
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: basePath,
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    outDir: `dist/sote-three`,
   }
 })
